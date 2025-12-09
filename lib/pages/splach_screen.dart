a83aa6932330459ba232b14102a096c9b1a4b4e0
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-10 01:52:48 +0200

    finish splach screen

--- lib/pages/splach_screen.dart
+++ lib/pages/splach_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_travel_app/pages/on_boarding_screen.dart';
 import 'package:world_travel_app/widgets/primary_color.dart';
 
 class SplachScreen extends StatefulWidget {
@@ -10,21 +11,49 @@ class SplachScreen extends StatefulWidget {
 
 class _SplachScreenState extends State<SplachScreen> {
   @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 2), () {
+      if (!mounted) return;
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (c) => OnBoardingScreen()),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primaryColor,
       body: Stack(
         children: [
           Positioned(
-            top: 100,
-            left: 120,
-            child: Image.asset('assets/splach_pic_1.png'),
+            top: 50,
+            left: 60,
+            child: Image.asset(
+              'assets/splach_pic_1.png',
+              width: 90,
+              height: 207,
+            ),
           ),
-          Center(child: Image.asset('assets/world_travel.png')),
+          Center(
+            child: Image.asset(
+              'assets/world_travel.png',
+              width: 300,
+              height: 150,
+              fit: BoxFit.contain,
+            ),
+          ),
           Positioned(
-            bottom: 100,
-            right: 120,
-            child: Image.asset('assets/splach_pic_2.png'),
+            bottom: 50,
+            right: 60,
+            child: Image.asset(
+              'assets/splach_pic_2.png',
+              width: 90,
+              height: 207,
+            ),
           ),
         ],
       ),
