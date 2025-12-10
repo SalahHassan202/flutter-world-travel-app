05349242c7e7ce41cb3273f2964f6ca41d646bdd
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-11 01:46:49 +0200

    update login screen

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,6 +11,22 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          children: [
+            Gap(80),
+            Image.asset(
+              'assets/auth.png',
+              width: 200,
+              height: 173,
+              fit: BoxFit.contain,
+            ),
+            Gap(15),
+          ],
+        ),
+      ),
+    );
   }
 }
