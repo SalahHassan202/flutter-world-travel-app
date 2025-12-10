c6407b89c23b0746e94c79182820d2308dad174d
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-10 14:06:49 +0200

    added custom text field widget

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String hintText;
+  final IconData suffixIcon;
+
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    required this.suffixIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyle(color: Colors.grey[500]),
+        filled: true,
+        fillColor: Colors.grey[200],
+        suffixIcon: Icon(suffixIcon),
+        border: OutlineInputBorder(),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey[200]!),
+          borderRadius: BorderRadius.circular(5),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey[200]!),
+          borderRadius: BorderRadius.circular(5),
+        ),
+      ),
+    );
+  }
+}
