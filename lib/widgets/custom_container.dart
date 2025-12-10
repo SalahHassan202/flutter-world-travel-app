f507282ce6dda981829b5d6e6397621d1fd31986
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-11 01:55:24 +0200

    added custom container

---
+++ lib/widgets/custom_container.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomContainer extends StatelessWidget {
+  final String text;
+  const CustomContainer({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 244,
+      height: 47,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Text(
+        text,
+        style: GoogleFonts.fredoka(
+          textStyle: TextStyle(
+            fontSize: 19,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
