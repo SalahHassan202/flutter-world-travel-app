8b8968f721d01a14ffab1cbb00a611ad99d46b93
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-11 02:43:42 +0200

    added padding for custom text field

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -12,21 +12,24 @@ class CustomTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: hintText,
-        hintStyle: TextStyle(color: Colors.grey[500]),
-        filled: true,
-        fillColor: Colors.grey[200],
-        suffixIcon: Icon(suffixIcon),
-        border: OutlineInputBorder(),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey[200]!),
-          borderRadius: BorderRadius.circular(5),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey[200]!),
-          borderRadius: BorderRadius.circular(5),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.grey[500]),
+          filled: true,
+          fillColor: Colors.grey[200],
+          suffixIcon: Icon(suffixIcon),
+          border: OutlineInputBorder(),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey[200]!),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey[200]!),
+            borderRadius: BorderRadius.circular(5),
+          ),
         ),
       ),
     );
