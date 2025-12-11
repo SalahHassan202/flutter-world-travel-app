0e9f881b0c0fdbec312180c8f2646fa9832a0d4a
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-11 03:13:19 +0200

    finish home screen

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:world_travel_app/widgets/custom_text.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          children: [
+            CustomText(text: 'Welcome to Home'),
+            Gap(40),
+            Image.asset('assets/home.png'),
+          ],
+        ),
+      ),
+    );
   }
 }
